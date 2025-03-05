ce3bdd0f371ad07df0703265bb8e6095566e0176
yash5333 <84564843+yash5333@users.noreply.github.com>
2025-03-05 10:29:57 +0530

    app.py

--- app.py
+++ app.py
@@ -307,6 +307,6 @@ def uploaded_file(filename):
 
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Use Render's port
+    app.run(host="0.0.0.0", port=port, debug=True)
